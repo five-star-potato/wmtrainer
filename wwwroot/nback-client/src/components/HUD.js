316ab59c1929380c61031ba3c21fd0e675f6577b
Cecil Lew <cecil.lew@gmail.com>
2017-11-21 07:23:06 -0500

    fixed scoring

--- wwwroot/nback-client/src/components/HUD.js
+++ wwwroot/nback-client/src/components/HUD.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
+import Glyphicon from 'react-bootstrap/lib/Glyphicon'
 import PropTypes from 'prop-types';
 import '../App.css';
 
@@ -14,17 +15,21 @@ const HUD = ({ gameStarted, options, numTrialsLeft, tpScore, fpScore, fnScore, p
             onStartClick(options);
         }
     }
+    let displayOptions = () => {
+
+    }
     return (
         <div className="row">
             <div className="col-md-5 col-lg-5 col-sm-6 col-xs-6">
                 <ButtonToolbar style={{ marginLeft: "0.5em" }}>
                     {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-                    <Button bsStyle={ gameStarted ? "danger" : "success"} onClick={ () => toggleGame() } >{btnText}</Button>
+                    <Button bsStyle={gameStarted ? "danger" : "success"} onClick={() => toggleGame()} >{btnText}</Button>
+                    <Button onClick={() => displayOptions()}><Glyphicon glyph="cog" /></Button>
                 </ButtonToolbar>
             </div>
             <div className="col-md-4 col-lg-4 col-sm-6 col-xs-6">
-                <p style={{ fontSize: "0.9em", marginLeft: "0.5em" }}>Trials left: {numTrialsLeft}<br/>
-                    <span style={{fontSize: "0.7em"}}>TP: {tpScore} FP: {fpScore} FN: {fnScore} PCT: {Math.round(pct)}</span></p>
+                <p style={{ fontSize: "0.9em", marginLeft: "0.5em" }}>Trials left: {numTrialsLeft}<br />
+                    <span style={{ fontSize: "0.7em" }}>TP: {tpScore} FP: {fpScore} FN: {fnScore} PCT: {Math.round(pct)}</span></p>
             </div>
         </div>
     );
